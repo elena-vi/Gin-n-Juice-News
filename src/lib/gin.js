3971bf46d18d9c4356c407dbf5a4d54c07b7914e
Elena <elena.vilimaite@gmail.com>
2016-07-05 12:52:48 +0100

    changing the way functions are run

--- src/lib/gin.js
+++ src/lib/gin.js
@@ -9,12 +9,12 @@ function router () {
     var url = location.hash.slice(1) || '/';
     var route = routes[url];
     if (view && route.controller) {
-    	route.controller();
     	var template = "../templates/"+route.template+".html";
     	var ajax = new XMLHttpRequest();
     	ajax.open("GET", template, false);
     	ajax.send();
     	view.innerHTML = ajax.responseText;
+        route.controller();
     }
 }
 // Listen on hash change:
