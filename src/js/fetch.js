f0000da246c8e0da0a98b8ee004219842a5d8774
Andy <andy.bell.github@gmail.com>
2016-07-07 19:46:19 +0100

    refactor of fetch

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -10,12 +10,7 @@ function Fetch() {
 
 };
 
-Fetch.prototype =  {
-
-  getData:  function(date) {
-    // Get me everything
-
-    var sourceURL = this.makersApiUrl + date + this.thumbnailUrl;
+function HttpRequest(sourceURL){
 
     return new Promise(function(resolve, reject) {
 
@@ -48,6 +43,14 @@ Fetch.prototype =  {
       // Make the  xmlHttpuest
       xmlHttp.send(null);
     });
+}
+
+Fetch.prototype =  {
+
+  getData:  function(date) {
+    // Get me everything
+    var sourceURL = this.makersApiUrl + date + this.thumbnailUrl;
+    return new HttpRequest(sourceURL)
 
   },
 
@@ -56,40 +59,10 @@ Fetch.prototype =  {
     //Get me the summary
 
     var sourceURL = this.summaryApiUrl + dataURL
-
-    return new Promise(function(resolve, reject) {
-
-      // Do the usual XHR stuff
-      var xmlHttp = new XMLHttpRequest();
-
-      xmlHttp.open('GET', sourceURL);
-
-      xmlHttp.onload = function() {
-        // This is called even on 404 etc
-        // so check the status
-        if ( xmlHttp.status == 200) {
-
-          // Resolve the promise with the response text
-          resolve(xmlHttp.responseText);
-
-        }
-        else {
-          // Otherwise reject with the status text
-          // which will hopefully be a meaningful error
-          reject(Error( xmlHttp.statusText));
-        }
-      };
-
-      // Handle network errors
-      xmlHttp.onerror = function() {
-        reject(Error("Network Error"));
-      };
-
-      // Make the  xmlHttpuest
-      xmlHttp.send(null);
-    });
+    return new HttpRequest(sourceURL)
 
   },
 
 
 };
+
