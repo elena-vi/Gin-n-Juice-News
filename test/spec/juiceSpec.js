d0efda1297e29001ee54d8bc9843c016551385a0
Elena <elena.vilimaite@gmail.com>
2016-07-07 15:35:42 +0100

    Juice testing

--- test/spec/juiceSpec.js
+++ test/spec/juiceSpec.js
@@ -1,10 +1,21 @@
 describe('Juice', function() {
 
-  beforeEach(function() {
-  });
+	it('A template get\'s complied', function() {
+		expect(juice('home',{snoopDogg: 'Chill, dis be a test'})).toContain('Chill, dis be a test');
+	})
 
-  it('Does a thing!', function() {
-    expect(true).toBeTruthy();
-  });
+	it('Translator removes syntax and separates variables from string', function () {
+		expect(translateTemplate('wiggle #{yo= cat } wiggle')).toEqual("wiggle ', cat ,' wiggle");
+	});
+
+	describe('Template peeler', function() {
+		it('Returns error when template is not found', function () {
+			expect(peelTemplate('badTemplate')).toEqual('Template error: 404');
+		});
+
+		it('Returns template as a string', function() {
+			expect(peelTemplate('home')).toContain('#{yo= snoopDogg}');
+		});
+	});
 
 });
